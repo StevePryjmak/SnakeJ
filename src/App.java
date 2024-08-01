2dd937d8b4be7e11303bc8c2de30d5428068183e
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-08-01 23:06:54 +0200

    implemented empty window initialization

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int Width = 600;
+        int Height = 600;
+
+        JFrame frame = new JFrame("Snake Game");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(Width, Height);
+        frame.setLocationRelativeTo(null);
+
+    }
+}
